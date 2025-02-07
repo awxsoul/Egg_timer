1b8c0ee4cc4512caffd4a3df4586eed9af0ba32c
Gopi Krishna <awxsoul8@gmail.com>
2025-02-08 00:31:56 +0530

    UI Page 01 Completed

---
+++ renderer.js
@@ -0,0 +1,11 @@
+console.log("working")
+
+const threeegg=document.getElementById("threeegg");
+const sixegg=document.getElementById("sixegg");
+const tenegg=document.getElementById("tenegg");
+
+
+threeegg.onclick=()=>{
+    threeegg.style.top="50%";
+    threeegg.style.left="48%"
+}
